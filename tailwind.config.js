ddd63ee24a5810c49a1da98462e3612a4ab66d8b
gift56 <efeasiughu@gmail.com>
2023-11-05 20:40:36 -0500

    working on a new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodyBg: "#0B0B0F",
+        cardDark: "#1A1B23",
+        gray1: "#898CA9",
+        gray2: "#B982FF",
+      },
+      backgroundImage: {
+        btnLinear: "linear-gradient(225deg, #18C8FF 14.89%, #933FFE 85.85%)",
+      },
+    },
   },
   plugins: [],
 };
