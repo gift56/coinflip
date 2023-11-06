167c3bdd148b962a06a923d570cc0c6f2e532c67
gift56 <efeasiughu@gmail.com>
2023-11-05 21:17:53 -0500

    working on a new project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,9 @@
   <header class="w-full sticky top-0 z-40 bg-transparent">
     <div class="container">
       <div class="w-full flex items-center justify-between gap-6 py-4">
-        <img src="../assets/image/logo.svg" alt="logo">
+        <RouterLink to="/">
+          <img src="../assets/image/logo.svg" alt="logo" />
+        </RouterLink>
       </div>
     </div>
   </header>
