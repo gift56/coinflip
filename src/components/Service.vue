0d7e1c2252c538a03e969e3c855811abec8ef660
gift56 <efeasiughu@gmail.com>
2023-11-06 18:50:08 -0500

    working on the next

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -4,7 +4,7 @@
       <div
         class="grid md:grid-cols-3 items-start justify-between gap-8 w-full py-20"
       >
-        <div class="card"></div>
+        <div class="flex flex-col items-center justify-center p-5 bg-cardDark rounded-3xl transition-all duration-300"></div>
       </div>
     </div>
   </section>
