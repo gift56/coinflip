f6a9ec66682d4c77772a0cfafef29454f9de7619
gift56 <efeasiughu@gmail.com>
2023-11-06 18:40:51 -0500

    working on the next

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from "../components/Hero.vue";
+import Service from "../components/Service.vue";
 </script>
 
 <template>
   <main>
     <Hero />
+    <Service />
   </main>
 </template>
