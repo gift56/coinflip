acb3d9032f6faaadecc9ea9e35d6e8ecc4d7efba
gift56 <efeasiughu@gmail.com>
2023-11-05 21:17:30 -0500

    working on a new project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-  <header>Header</header>
+  <header class="w-full sticky top-0 z-40 bg-transparent">
+    <div class="container">
+      <div class="w-full flex items-center justify-between gap-6 py-4">
+        <img src="../assets/image/logo.svg" alt="logo">
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
