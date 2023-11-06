59701b320e9bfef5856f90103faf998b4ce9d8af
gift56 <efeasiughu@gmail.com>
2023-11-06 10:18:52 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,11 +2,17 @@
   <section
     className="w-full relative h-screen flex flex-col items-center justify-center"
   >
-    <div class="flex flex-col w-full items-center justify-center gap-8 relative">
+    <div
+      class="flex flex-col w-full items-center justify-center gap-8 relative"
+    >
       <h1 class="text-3xl font-extrabold md:text-6xl lg:text-7xl text-center">
         We make crypto <br class="hidden lg:block" />
         clear and simple
       </h1>
+      <p class="">
+        Buy, sell, and grow your crypto with CoinFlip, the platform dedicated
+        to every trader at every level.
+      </p>
     </div>
   </section>
 </template>
