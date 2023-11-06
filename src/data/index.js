ce46e6570812913861900757cde9a900648f9eb8
gift56 <efeasiughu@gmail.com>
2023-11-06 04:42:32 -0500

    working on data mapping

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,6 @@
-
+import cardIcon1 from "../assets/icon/cardIcon1.png";
+import cardIcon2 from "../assets/icon/cardIcon2.png";
+import cardIcon3 from "../assets/icon/cardIcon3.png";
 
 export const cardData = [
   {
