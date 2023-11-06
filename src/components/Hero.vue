9eee3e0e6bf05c5c0872b0ff80296547954c758e
gift56 <efeasiughu@gmail.com>
2023-11-06 10:18:20 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
   <section
     className="w-full relative h-screen flex flex-col items-center justify-center"
   >
-    <div class="flex flex-col w-full items-center justify-center gap-8">
+    <div class="flex flex-col w-full items-center justify-center gap-8 relative">
       <h1 class="text-3xl font-extrabold md:text-6xl lg:text-7xl text-center">
         We make crypto <br class="hidden lg:block" />
         clear and simple
