e812a9917ce45fa4d03cc3f9734a6741cabc7de4
gift56 <efeasiughu@gmail.com>
2023-11-06 04:44:33 -0500

    working on data mapping

--- src/data/index.js
+++ src/data/index.js
@@ -4,6 +4,21 @@ import cardIcon3 from "../assets/icon/cardIcon3.png";
 
 export const cardData = [
   {
-    icon: "",
+    icon: cardIcon1,
+    title: "Trade Desk",
+    desc: "Invest in crypto anytime, anywhere with our safe, secure, and easy to use online platform",
+    linkText: "Get Started",
+  },
+  {
+    icon: cardIcon2,
+    title: "CoinFlip ATMs",
+    desc: "We have thousands of ATMs located across the U.S. where you can easily convert cash to crypto",
+    linkText: "Find an ATM",
+  },
+  {
+    icon: cardIcon3,
+    title: "CoinFlip Wallet",
+    desc: "Store your growing investments in our non-custodial wallet that gives you access to a full suite of DeFi services in one place",
+    linkText: "Download the App",
   },
 ];
