45047c6a92c6e7bacd89ba58b8b79d66fe38e0df
gift56 <efeasiughu@gmail.com>
2023-11-06 10:16:10 -0500

    working on hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,9 @@
   <section
     className="w-full relative h-screen flex flex-col items-center justify-center"
   >
-    <div class="flex flex-col w-full h-full"></div>
+    <div class="flex flex-col w-full items-center justify-center  gap-8">
+
+    </div>
   </section>
 </template>
 
