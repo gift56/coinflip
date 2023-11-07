23d57ccc40ac5f71aff7f19cbb433b8b66503c88
gift56 <efeasiughu@gmail.com>
2023-11-07 04:18:23 -0500

    working on next section

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -3,8 +3,11 @@
     <div class="container">
       <div class="flex flex-col items-center justify-center gap-8 py-20 w-full">
         <div class="flex flex-col items-center justify-center gap-4 w-full">
-          <h3 class="text-3xl font-extrabold md:text-5xl">
-            Buy and sell with the lowest fees in the industry
+          <h3
+            class="text-3xl font-extrabold md:text-5xl flex flex-col items-center justify-center text-center"
+          >
+            <span>Buy and sell with the lowest</span>
+            <span>fees in the industry</span>
           </h3>
         </div>
       </div>
