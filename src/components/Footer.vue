fc863ac5ddfc37e6983711ce2e4cb24e1e6ca063
gift56 <efeasiughu@gmail.com>
2023-11-07 05:14:25 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,14 @@
 <template>
   <footer class="w-full border-t border-[#2B2C3B] relative">
     <div class="container">
-      <div class="w-full py-20 flex flex-col items-start justify-between md:flex-row gap-8">
-        <div class="w-full"></div>
+      <div
+        class="w-full py-20 flex flex-col items-start justify-between md:flex-row gap-8"
+      >
+        <div class="w-full flex flex-col items-start justify-start gap-4">
+          <RouterLink to="/">
+            <img src="@/assets/image/logo.svg" alt="logo" />
+          </RouterLink>
+        </div>
         <div class="w-full"></div>
         <div class="w-full"></div>
       </div>
