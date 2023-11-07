862df87457229a1891698abc0d9048f3d88f5c72
gift56 <efeasiughu@gmail.com>
2023-11-06 19:08:26 -0500

    working on the next

--- src/components/Invest.vue
+++ src/components/Invest.vue
@@ -1,7 +1,28 @@
 <template>
-  <section>Invest</section>
+  <section class="w-full">
+    <div class="container">
+      <div class="flex flex-col w-full items-center justify-center gap-8 py-20">
+        <h1 class="text-3xl font-extrabold md:text-6xl text-center">
+          A crypto investment <br class="hidden lg:block" />
+          platform that invests in you
+        </h1>
+        <p
+          class="lg:max-w-[580px] text-center text-base font-normal text-gray1"
+        >
+          We invest more resources than any other platrom in making sure great
+          support from real people is a click away, whenever you need it.
+        </p>
+        <RouterLink to="/">
+          <button
+            type="button"
+            class="w-fit px-6 py-3 rounded-xl bg-btnLinear text-white outline-none text-lg font-semibold"
+          >
+            Get Started
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-export default {};
-</script>
+<script></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Hero from "../components/Hero.vue";
 import Service from "../components/Service.vue";
+import Invest from "../components/Invest.vue";
 </script>
 
 <template>
   <main>
     <Hero />
     <Service />
+    <Invest />
   </main>
 </template>
