87b2fce7e2cebcd05618dab919a850c7a6642d20
gift56 <efeasiughu@gmail.com>
2023-11-07 05:25:31 -0500

    done working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,11 +4,11 @@
       <div
         class="w-full py-20 flex flex-col items-start justify-between md:flex-row gap-8"
       >
-        <div class="w-full flex flex-col items-start justify-start gap-4">
+        <div class="w-full flex flex-col items-start justify-start gap-6">
           <RouterLink to="/">
             <img src="@/assets/image/logo.svg" alt="logo" />
           </RouterLink>
-          <p class="text-sm font-normal text-white">
+          <p class="text-sm font-normal text-white max-w-[400px]">
             CoinFlip, the world’s leading bitcoin ATM operator, makes it so
             flippin’ easy to buy and sell bitcoin via cash, card, or bank
             transfer. Sign up to get the latest in CoinFlip news, discounts, and
@@ -18,31 +18,33 @@
             © 2021 GPD Holdings, LLC FinCEN MSB
           </p>
         </div>
-        <div class="w-full flex flex-col items-start justify-start gap-3">
-          <h3 class="text-sm font-bold text-white">Company</h3>
-          <ul class="w-full flex flex-col items-start justify-start gap-4">
-            <li
-              v-for="(text, index) in links"
-              :key="index"
-              class="cursor-pointer text-sm font-normal text-gray1"
-            >
-              {{ text }}
-            </li>
-          </ul>
-        </div>
-        <div class="w-full flex flex-col items-start justify-start gap-3">
-          <h3 class="text-sm font-bold text-white">
-            Privacy Policy and Terms of Service
-          </h3>
-          <ul class="w-full flex flex-col items-start justify-start gap-4">
-            <li
-              v-for="(text, index) in termlinks"
-              :key="index"
-              class="cursor-pointer text-sm font-normal text-gray1"
-            >
-              {{ text }}
-            </li>
-          </ul>
+        <div class="w-full flex flex-col gap-6 lg:flex-row items-start">
+          <div class="w-full flex flex-col items-start justify-start gap-6">
+            <h3 class="text-sm font-bold text-white">Company</h3>
+            <ul class="w-full flex flex-col items-start justify-start gap-4">
+              <li
+                v-for="(text, index) in links"
+                :key="index"
+                class="cursor-pointer text-sm font-normal text-gray1"
+              >
+                {{ text }}
+              </li>
+            </ul>
+          </div>
+          <div class="w-full flex flex-col items-start justify-start gap-6">
+            <h3 class="text-sm font-bold text-white">
+              Privacy Policy and Terms of Service
+            </h3>
+            <ul class="w-full flex flex-col items-start justify-start gap-4">
+              <li
+                v-for="(text, index) in termlinks"
+                :key="index"
+                class="cursor-pointer text-sm font-normal text-gray1"
+              >
+                {{ text }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
