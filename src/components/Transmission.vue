e3dc530118c6f95db5ce37fbc94181bd6ca8f8a9
gift56 <efeasiughu@gmail.com>
2023-11-07 05:08:09 -0500

    working on next section

--- src/components/Transmission.vue
+++ src/components/Transmission.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="w-full">
     <div class="container">
-      <div class="w-full flex flex-col items-center justify-center gap-6">
+      <div class="w-full flex flex-col items-center justify-center gap-6 py-14">
         <h3 class="text-3xl font-extrabold md:text-5xl text-center">
           Receive transmissions
         </h3>
@@ -10,7 +10,7 @@
           <RouterLink to="/" class="text-white"> Privacy policy↗ </RouterLink>
         </p>
         <div
-          class="w-full h-14 md:w-[304px] shadow-inputshad rounded-lg border border-[#18C8FF] flex items-center justify-between bg-bodyBg"
+          class="w-full h-14 md:w-[304px] shadow-inputshad rounded-lg border border-[#18C8FF] flex items-center justify-between bg-bodyBg px-4"
         >
           <input type="text" placeholder="Email Address" class="w-full h-full outline-none text-sm font-normal text-gray1 bg-transparent" />
           <img src="../assets/icon/arrowIcon.png" alt="arrow" />
