47cbdede8035eac6a62363acb5323c160ea7acf3
gift56 <efeasiughu@gmail.com>
2023-11-07 05:19:00 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,14 +11,24 @@
           <p class="text-sm font-normal text-white">
             CoinFlip, the world’s leading bitcoin ATM operator, makes it so
             flippin’ easy to buy and sell bitcoin via cash, card, or bank
-            transfer. Sign up to get the latest in CoinFlip news, discounts,
-            and more.
+            transfer. Sign up to get the latest in CoinFlip news, discounts, and
+            more.
           </p>
-          <p class="text-sm font-normal text-gray1">© 2021 GPD Holdings, LLC FinCEN MSB</p>
+          <p class="text-sm font-normal text-gray1">
+            © 2021 GPD Holdings, LLC FinCEN MSB
+          </p>
         </div>
         <div class="w-full flex flex-col items-start justify-start gap-3">
           <h3 class="text-sm font-bold text-white">Company</h3>
-          <ul class="w-full flex flex-col items-start justify-start gap-4"></ul>
+          <ul class="w-full flex flex-col items-start justify-start gap-4">
+            <li
+              v-for="(text, index) in links"
+              :key="index"
+              class="cursor-pointer text-sm font-normal text-gray1"
+            >
+              {{ text }}
+            </li>
+          </ul>
         </div>
         <div class="w-full"></div>
       </div>
