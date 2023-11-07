68cbf25f7f190f0a00fe578ab2e76a361dc83c20
gift56 <efeasiughu@gmail.com>
2023-11-07 14:36:23 -0500

    working on some updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -39,6 +39,7 @@
       />
       <img src="../assets/image/heroImage1.png" alt="coin" class="absolute top-[70%] lg:top-[-2%] left-[2%] lg:left-[5%] w-14 h-14 md:w-32 md:h-32" />
       <img src="../assets/image/heroImage2.png" alt="coin" class="absolute top-[60%] left-[5%] w-28 h-28 hidden lg:block" />
+      <img src="../assets/image/heroImage3.png" alt="coin" class="absolute top-[95%] left-[20%] w-28 h-28 hidden lg:block" />
     </div>
   </section>
 </template>
