6a7bdc40563d0a3f30b27ff45983932f71867a02
gift56 <efeasiughu@gmail.com>
2023-11-07 04:53:08 -0500

    working on next section

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -4,25 +4,25 @@
       <div
         class="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8 relative py-20"
       >
-        <div class="w-full lg:flex-1">
-          <img src="../assets/image/caseImage.png" alt="shieldImage" />
-        </div>
         <div
           class="w-full lg:flex-1 flex flex-col items-start justify-start gap-6"
         >
           <h3 class="text-3xl font-extrabold md:text-5xl">
-            24/7 access to full service customer support
+            Take your first step into safe, secure crypto investing
           </h3>
           <p class="text-base text-gray1 font-normal">
-            We invest more resources than any other platform in making sure
-            great support from real people is a click away, whenever you need
-            it.
+            Separated they live in Bookmarks right at the coast of the famous
+            Semantics, large language ocean Separated they live in Bookmarks
+            right at the coast.
           </p>
           <RouterLink
             to="/"
             class="text-lg font-semibold border w-fit px-4 py-2 rounded-xl"
             >Get Started</RouterLink
           >
+        </div>
+        <div class="w-full lg:flex-1">
+          <img src="../assets/image/caseImage.png" alt="shieldImage" />
         </div>
       </div>
     </div>
