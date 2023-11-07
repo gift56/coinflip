cc6b950e04ef1c6404541058b4f60b877c49d642
gift56 <efeasiughu@gmail.com>
2023-11-07 17:18:34 -0500

    working on some animation feature

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,16 +5,47 @@
     <div
       class="flex flex-col w-full items-center justify-center gap-8 relative z-10 py-6"
     >
-      <h1 class="text-3xl font-extrabold md:text-6xl lg:text-7xl text-center">
+      <h1
+        v-motion
+        :initial="{ opacity: 0, y: -150 }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 100,
+          },
+        }"
+        class="text-3xl font-extrabold md:text-6xl lg:text-7xl text-center"
+      >
         We make crypto <br class="hidden lg:block" />
         clear and simple
       </h1>
-      <p class="lg:max-w-[580px] text-center text-base font-normal text-gray1">
+      <p
+        v-motion
+        :initial="{ opacity: 0, y: 50 }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 100,
+          },
+        }"
+        class="lg:max-w-[580px] text-center text-base font-normal text-gray1"
+      >
         Buy, sell, and grow your crypto with CoinFlip, the platform dedicated to
         every trader at every level.
       </p>
       <RouterLink to="/">
         <button
+          v-motion
+          :initial="{ opacity: 0, scale: 0 }"
+          :enter="{
+            opacity: 1,
+            scale: 1,
+            transition: {
+              delay: 100,
+            },
+          }"
           type="button"
           class="w-fit px-6 py-3 rounded-xl bg-btnLinear text-white outline-none text-lg font-semibold"
         >
@@ -38,16 +69,43 @@
         class="absolute top-[-25%]"
       />
       <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0 }"
+        :enter="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 100,
+          },
+        }"
         src="../assets/image/heroImage1.png"
         alt="coin"
         class="absolute top-[70%] lg:top-[-2%] left-[2%] lg:left-[5%] w-14 h-14 md:w-32 md:h-32"
       />
       <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0 }"
+        :enter="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 60,
+          },
+        }"
         src="../assets/image/heroImage2.png"
         alt="ball"
         class="absolute top-[60%] left-[5%] w-28 h-28 hidden lg:block"
       />
       <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0 }"
+        :enter="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 90,
+          },
+        }"
         src="../assets/image/heroImage3.png"
         alt="coin"
         class="absolute top-[95%] left-[20%] w-28 h-28 hidden lg:block"
@@ -58,11 +116,29 @@
         class="absolute top-[5%] right-[15%] w-16 h-16 hidden lg:block animate-pulse"
       />
       <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0 }"
+        :enter="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 100,
+          },
+        }"
         src="../assets/image/heroImage5.png"
         alt="coin"
         class="absolute top-[26%] right-0 lg:right-[10%] w-14 h-14 md:w-28 md:h-28"
       />
       <img
+        v-motion
+        :initial="{ opacity: 0, scale: 0 }"
+        :enter="{
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delay: 120,
+          },
+        }"
         src="../assets/image/heroImage6.png"
         alt="coin"
         class="absolute top-[70%] right-0 lg:right-[13%] w-14 h-14 md:w-28 md:h-28"
