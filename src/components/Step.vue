1ee16470bc3fc6ff0767d2cbf16fcb358edfab4b
gift56 <efeasiughu@gmail.com>
2023-11-07 18:03:05 -0500

    working on some animation feature

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -7,22 +7,64 @@
         <div
           class="w-full lg:flex-1 flex flex-col items-start justify-start gap-6"
         >
-          <h3 class="text-3xl font-extrabold md:text-5xl">
+          <h3
+            v-motion
+            :initial="{ opacity: 0, y: 50 }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 100,
+              },
+            }"
+            class="text-3xl font-extrabold md:text-5xl"
+          >
             Take your first step into safe, secure crypto investing
           </h3>
-          <p class="text-base text-gray1 font-normal">
+          <p
+            v-motion
+            :initial="{ opacity: 0, y: 50 }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 100,
+              },
+            }"
+            class="text-base text-gray1 font-normal"
+          >
             Separated they live in Bookmarks right at the coast of the famous
             Semantics, large language ocean Separated they live in Bookmarks
             right at the coast.
           </p>
           <RouterLink
             to="/"
+            v-motion
+            :initial="{ opacity: 0, scale: 0 }"
+            :visible="{
+              opacity: 1,
+              scale: 1,
+              transition: {
+                delay: 100,
+              },
+            }"
             class="w-fit px-6 py-3 rounded-xl bg-btnLinear text-white outline-none text-lg font-semibold"
             >Get Started</RouterLink
           >
         </div>
-        <div class="w-full lg:flex-1 flex items-center justify-center relative">
+        <div
+          class="w-full lg:flex-1 flex items-center justify-center relative"
+        >
           <img
+            v-motion
+            :initial="{ opacity: 0, x: 150 }"
+            :visible="{
+              opacity: 1,
+              x: 0,
+              transition: {
+                delay: 100,
+              },
+            }"
             src="../assets/image/caseImage.png"
             alt="shieldImage"
             class="relative z-20"
