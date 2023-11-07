04023bbbd523af988052a577ac11f845a5893745
gift56 <efeasiughu@gmail.com>
2023-11-07 04:16:11 -0500

    working on next section

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="w-full">
     <div class="container">
-      
+      <div class="flex flex-col items-center justify-center gap-8 py-20 w-full">
+        
+      </div>
     </div>
   </section>
 </template>
