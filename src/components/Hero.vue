888414c7f1b944c81653629b79d403cd6ca1107e
gift56 <efeasiughu@gmail.com>
2023-11-07 14:46:53 -0500

    working on some updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -40,12 +40,12 @@
       <img src="../assets/image/heroImage1.png" alt="coin" class="absolute top-[70%] lg:top-[-2%] left-[2%] lg:left-[5%] w-14 h-14 md:w-32 md:h-32" />
       <img src="../assets/image/heroImage2.png" alt="ball" class="absolute top-[60%] left-[5%] w-28 h-28 hidden lg:block" />
       <img src="../assets/image/heroImage3.png" alt="coin" class="absolute top-[95%] left-[20%] w-28 h-28 hidden lg:block" />
-      <img src="../assets/image/heroImage4.png" alt="star" class="absolute top-[5%] right-[15%] w-16 h-16 hidden lg:block" />
+      <img src="../assets/image/heroImage4.png" alt="star" class="absolute top-[5%] right-[15%] w-16 h-16 hidden lg:block animate-pulse" />
       <img src="../assets/image/heroImage5.png" alt="coin" class="absolute top-[26%] right-[10%] w-28 h-28 hidden lg:block" />
       <img src="../assets/image/heroImage6.png" alt="coin" class="absolute top-[70%] right-[13%] w-28 h-28 hidden lg:block" />
-      <img src="../assets/image/heroImage7.png" alt="coin" class="absolute top-[60%] right-[0%] w-40 h-60 hidden lg:block" />
-      <img src="../assets/image/heroImage8.png" alt="coin" class="absolute top-[80%] right-[30%] w-10 h-10 hidden lg:block animate-pulse" />
-      <img src="../assets/image/heroImage8.png" alt="coin" class="absolute top-[25%] delay-300 left-[16%] w-10 h-10 hidden lg:block animate-pulse" />
+      <img src="../assets/image/heroImage7.png" alt="ball" class="absolute top-[60%] right-[0%] w-40 h-60 hidden lg:block" />
+      <img src="../assets/image/heroImage8.png" alt="star" class="absolute top-[80%] right-[30%] w-10 h-10 hidden lg:block animate-pulse" />
+      <img src="../assets/image/heroImage8.png" alt="star" class="absolute top-[25%] delay-300 left-[16%] w-10 h-10 hidden lg:block animate-pulse" />
     </div>
   </section>
 </template>
