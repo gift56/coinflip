65346726a5fa80f02a355f1a82c543b20221f2db
gift56 <efeasiughu@gmail.com>
2023-11-07 04:13:37 -0500

    working on next section

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -2,12 +2,12 @@
   <section class="w-full">
     <div class="container">
       <div
-        class="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8 relative"
+        class="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8 relative py-20"
       >
         <div class="w-full lg:flex-1">
           <img src="../assets/image/shieldImage.png" alt="shieldImage" />
         </div>
-        <div class="w-full lg:flex-1 flex flex-col items-start justify-start">
+        <div class="w-full lg:flex-1 flex flex-col items-start justify-start gap-4">
           <h3 class="text-3xl font-extrabold md:text-5xl">
             24/7 access to full service customer support
           </h3>
