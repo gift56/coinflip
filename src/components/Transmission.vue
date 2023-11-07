d1a91896bf6c1757b28b63f65fdcfc2b4beec6ac
gift56 <efeasiughu@gmail.com>
2023-11-07 04:58:15 -0500

    working on next section

--- src/components/Transmission.vue
+++ src/components/Transmission.vue
@@ -1,5 +1,11 @@
 <template>
-  <section class="w-full">Transmission</section>
+  <section class="w-full">
+    <div class="container">
+      <div class="w-full flex flex-col items-center justify-center gap-6">
+        <h3>Receive transmissions</h3>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
