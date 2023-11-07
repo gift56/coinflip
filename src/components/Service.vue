b01e27113b9891329b52379f05845172345096e6
gift56 <efeasiughu@gmail.com>
2023-11-07 04:10:01 -0500

    working on next section

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,6 +1,15 @@
 <template>
   <section class="w-full">
-    <div class="container"></div>
+    <div class="container">
+      <div
+        class="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8"
+      >
+        <div class="w-full lg:flex-1">
+          <img src="../assets/image/shieldImage.png" alt="shieldImage" />
+        </div>
+        <div class="w-full lg:flex-1"></div>
+      </div>
+    </div>
   </section>
 </template>
 
