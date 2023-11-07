9f8537f5ca8bacf8aeb409c0351e5d479756a0d6
gift56 <efeasiughu@gmail.com>
2023-11-07 04:52:13 -0500

    working on next section

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -1,7 +1,32 @@
 <template>
-  <section>Secure</section>
+  <section class="w-full">
+    <div class="container">
+      <div
+        class="w-full flex flex-col lg:flex-row items-start justify-start lg:justify-between gap-8 relative py-20"
+      >
+        <div class="w-full lg:flex-1">
+          <img src="../assets/image/caseImage.png" alt="shieldImage" />
+        </div>
+        <div
+          class="w-full lg:flex-1 flex flex-col items-start justify-start gap-6"
+        >
+          <h3 class="text-3xl font-extrabold md:text-5xl">
+            24/7 access to full service customer support
+          </h3>
+          <p class="text-base text-gray1 font-normal">
+            We invest more resources than any other platform in making sure
+            great support from real people is a click away, whenever you need
+            it.
+          </p>
+          <RouterLink
+            to="/"
+            class="text-lg font-semibold border w-fit px-4 py-2 rounded-xl"
+            >Get Started</RouterLink
+          >
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup></script>
