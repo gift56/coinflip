5c40edd4edfc44ed0f075e91b1b59542e3309c2d
gift56 <efeasiughu@gmail.com>
2023-11-07 08:22:47 -0500

    perfecting everything

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="w-full">
-    <div class="container">
+    <div class="container relative z-10">
       <div
         class="grid md:grid-cols-3 items-start justify-between gap-8 w-full py-20"
       >
