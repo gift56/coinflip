414d50388d7c5020c16226fe545e813f7b483d45
gift56 <efeasiughu@gmail.com>
2023-11-07 05:12:28 -0500

    working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="w-full border-t border-[#2B2C3B] relative">
     <div class="container">
-      <div class="w-full py-20 flex flex-col items-start justify-between md:flex-row"></div>
+      <div class="w-full py-20 flex flex-col items-start justify-between md:flex-row gap-8"></div>
     </div>
   </footer>
 </template>
