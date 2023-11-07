7eb590d841fc7509b64131e73be182ad4473a6fc
gift56 <efeasiughu@gmail.com>
2023-11-07 18:04:05 -0500

    working on some animation feature

--- src/components/Transmission.vue
+++ src/components/Transmission.vue
@@ -2,17 +2,52 @@
   <section class="w-full">
     <div class="container">
       <div class="w-full flex flex-col items-center justify-center gap-6 py-14">
-        <h3 class="text-3xl font-extrabold md:text-5xl text-center">
+        <h3
+          v-motion
+          :initial="{ opacity: 0, y: 50 }"
+          :visible="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: 100,
+            },
+          }"
+          class="text-3xl font-extrabold md:text-5xl text-center"
+        >
           Receive transmissions
         </h3>
-        <p class="text-sm md:text-base text-gray1 font-normal">
+        <p
+          v-motion
+          :initial="{ opacity: 0, y: 50 }"
+          :visible="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              delay: 100,
+            },
+          }"
+          class="text-sm md:text-base text-gray1 font-normal"
+        >
           Unsubscribe at any time.
           <RouterLink to="/" class="text-white"> Privacy policy↗ </RouterLink>
         </p>
         <div
+          v-motion
+          :initial="{ opacity: 0, x: -150 }"
+          :visible="{
+            opacity: 1,
+            x: 0,
+            transition: {
+              delay: 100,
+            },
+          }"
           class="w-full h-14 md:w-[304px] shadow-inputshad rounded-lg border border-[#18C8FF] flex items-center justify-between bg-bodyBg px-4"
         >
-          <input type="text" placeholder="Email Address" class="w-full h-full outline-none text-sm font-normal text-gray1 bg-transparent" />
+          <input
+            type="text"
+            placeholder="Email Address"
+            class="w-full h-full outline-none text-sm font-normal text-gray1 bg-transparent"
+          />
           <img src="../assets/icon/arrowIcon.png" alt="arrow" />
         </div>
       </div>
