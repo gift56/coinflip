28cdb824cf97e30424682d6c7c0fc14f3948fe39
gift56 <efeasiughu@gmail.com>
2023-11-07 05:10:48 -0500

    working on next section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-  <footer class="w-full border-t border-[#2B2C3B]">Fopter</footer>
+  <footer class="w-full border-t border-[#2B2C3B]">
+    <div class="container">
+      <div></div>
+    </div>
+  </footer>
 </template>
 
 <script setup></script>
