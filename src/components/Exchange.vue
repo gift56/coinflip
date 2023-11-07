25056db4a4154dff352b387dbc696ce34717b693
gift56 <efeasiughu@gmail.com>
2023-11-07 04:49:46 -0500

    working on next section

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col items-center justify-center gap-8 py-20 w-full">
         <div class="flex flex-col items-center justify-center gap-4 w-full">
           <h3
-            class="text-3xl font-extrabold md:text-5xl flex flex-col items-center justify-center text-center"
+            class="text-3xl font-extrabold md:text-5xl lg:flex lg:flex-col items-center justify-center text-center"
           >
             <span>Buy and sell with the lowest</span>
             <span>fees in the industry</span>
@@ -28,29 +28,29 @@
           </RouterLink>
         </div>
         <div
-          class="flex flex-col items-start justify-start bg-cardDark rounded-3xl transition-all duration-300 w-full"
+          class="flex flex-col px-3 items-start justify-start bg-cardDark rounded-3xl transition-all duration-300 w-full"
         >
           <div
             v-for="graphInfo in graphData"
             :key="graphInfo.coinName"
-            class="w-full py-3 px-2 border-b border-gray1 last:border-none grid grid-cols-2 lg:flex items-center justify-between"
+            class="w-full py-3 px-2 border-b border-[#2B2C3B] last:border-none grid gap-4 lg:gap-0 grid-cols-2 lg:flex items-center justify-between"
           >
             <div class="w-full">
               <h4 class="text-lg font-bold md:text-xl text-white">
                 {{ graphInfo.coinName }}
               </h4>
             </div>
-            <div class="w-full flex items-center justify-center">
+            <div class="w-full flex items-start justify-start">
               <h5 class="text-lg font-bold md:text-xl text-gray2">
                 {{ graphInfo.prefix }}
               </h5>
             </div>
-            <div class="w-full flex items-end justify-end">
+            <div class="w-full flex lg:items-end lg:justify-end">
               <h5 class="text-lg font-bold md:text-xl text-white">
                 {{ graphInfo.rate }}
               </h5>
             </div>
-            <div class="w-full flex items-end justify-end">
+            <div class="w-full flex lg:items-end lg:justify-end">
               <h6
                 class="text-lg font-bold md:text-xl"
                 :class="
@@ -62,8 +62,8 @@
                 {{ graphInfo.percentage }}
               </h6>
             </div>
-            <div class="w-full">
-              <img :src="graphInfo.graphImage" :alt="graphInfo.coinName" />
+            <div class="w-full flex lg:items-center lg:justify-center pr-4 lg:pr-0 lg:pl-4">
+              <img :src="graphInfo.graphImage" :alt="graphInfo.coinName" class="w-full object-contain" />
             </div>
             <div class="w-full">
               <RouterLink
